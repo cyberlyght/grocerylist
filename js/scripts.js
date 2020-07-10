07cd39cc35d1d83f2e2c0fba41250b0b88ec1509
David Sampson <cyberlyght@gmail.com>
2020-07-10 19:30:44 -0400

    Version 2 - with animations

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@
  * Date: 07/10/2020
  */
 
+// Parse JSON response and add elements to the page.
 function loadGroceryList() {
 
 	if (this.readyState === XMLHttpRequest.DONE) {
@@ -11,15 +12,22 @@ function loadGroceryList() {
 			const groceryList = JSON.parse(this.response);
 			let tilesElement = document.getElementById('groceryList');
 
+			// Create grocery item tiles for each item in the JSON response and add them to the tiles element.
 			groceryList.forEach(function(item, index){
+
 				let groceryItemHTML = '<div class="groceryItemQuantity accent">QTY: ' + item.qty + '</div>';
 				groceryItemHTML += '<div class="groceryItemName">' + item.item + '</div>';
 				groceryItemHTML += '<div class="groceryItemDescription">' + item.brand + ' ' + item.type + '</div>';
 				groceryItemHTML += '<div class="groceryItemCategory accent">' + item.category + '</div>';
 				
 				let groceryItemElement = document.createElement('div');
-				groceryItemElement.setAttribute('class', 'groceryItem');
+				groceryItemElement.setAttribute('class', 'groceryItem expandInRight');
 				groceryItemElement.innerHTML = groceryItemHTML;
+
+				// Trigger animation on the grocery item element after a delay.
+				setTimeout(function(){
+					groceryItemElement.classList.remove('expandInRight');
+				}, index*100+10);
 
 				let tileElement = document.createElement('li');
 				tileElement.setAttribute('class', 'tile');
@@ -38,6 +46,7 @@ function loadGroceryList() {
 }
 
 
+// Get specified JSON file and execute callback function.
 function getJSON(filePath, callback) {
 
 	let httpRequest = new XMLHttpRequest();
@@ -54,6 +63,7 @@ function getJSON(filePath, callback) {
 }
 
 
+// If document is already ready, execute getJSON now, otherwise, execute it on ready.
 if (document.readyState === 'loading') {
 
 	document.addEventListener('DOMContentLoaded', function () {
