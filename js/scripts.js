37f8f156db26fd90eaf8a267367181d96543bf46
David Sampson <cyberlyght@gmail.com>
2020-07-10 17:09:02 -0400

    Code formatting

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,59 @@
 function loadGroceryList() {
+
 	if (this.readyState === XMLHttpRequest.DONE) {
 		if (this.status === 200) {
-			const groceryListObject = this.response;
-			console.log('groceryListObject');
+
+			const groceryList = this.response;
+			let tilesElement = document.getElementById('groceryList');
+
+			groceryList.forEach(function(item){
+				let groceryItemHTML = '<div class="groceryItem">';
+				groceryItemHTML += '<div class="groceryItemQuantity">QTY: ' + item.qty + '</div>';
+				groceryItemHTML += '<div class="groceryItemName">' + item.item + '</div>';
+				groceryItemHTML += '<div class="groceryItemDescription">' + item.brand + ' ' + item.type + '</div>';
+				groceryItemHTML += '<div class="groceryItemCategory">' + item.category + '</div>';
+				groceryItemHTML += '</div>';
+
+				let tileElement = document.createElement('div');
+				tileElement.setAttribute('class', 'tile');
+				tileElement.innerHTML = groceryItemHTML;
+
+				tilesElement.appendChild(tileElement);
+			});
+
 		} else {
+
 			alert('Could not load grocery list.');
+			
 		}
 	}
+
 }
 
 function getJSON(filePath, callback) {
-	var httpRequest = new XMLHttpRequest();
+
+	let httpRequest = new XMLHttpRequest();
 
 	if (!httpRequest) {
 		alert('Cannot create an XMLHTTP instance');
 		return false;
 	}
+
 	httpRequest.onreadystatechange = callback;
 	httpRequest.open('GET', filePath);
 	httpRequest.responseType = 'json';
 	httpRequest.send();
+
 }
 
 if (document.readyState === 'loading') {
+
 	document.addEventListener('DOMContentLoaded', function () {
 		getJSON('data/groceries.json', loadGroceryList);
 	});
+
 } else {
+
 	getJSON('data/groceries.json', loadGroceryList);
+
 }
